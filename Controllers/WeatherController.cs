e3ba04099ebd898aee623b4c6e8b3adb9a8853be
ndumisoz <ndumisoz@NDUMISOZ-SSD>
2023-04-14 16:28:40 +0200

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Individual_Project_DevExtreme.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Individual_Project_DevExtreme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
